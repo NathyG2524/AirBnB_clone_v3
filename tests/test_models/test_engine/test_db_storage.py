2a0ea297411371cfa68c85446f3f1308e4d2fb43
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-16 04:13:11 -0700

    modify

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,7 +86,11 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self, cls, id):
         """ Test that gets an obj"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self, cls):
         """ Test that count the object created""
