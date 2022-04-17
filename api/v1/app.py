ae6d80ef24b9be6fef4be26aaaa5fab6e3c7aa30
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-17 09:32:46 -0700

    add method to handle not ound

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 """
-first endpoint (route) will be to 
+first endpoint (route) will be to
 return the status of your API
 """
-
 
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import make_response
+from flask import jsonify
 import os
+
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-     Registers a function to be called 
-    when the application context ends. 
+    Registers a function to be called
+    when the application context ends.
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Not found """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     if os.getenv('HBNB_API_HOST') and os.getenv('HBNB_API_PORT'):
