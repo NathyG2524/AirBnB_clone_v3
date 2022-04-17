8d5eae46777bfda5f930ca59875c7c2bcdb67f54
NathyG2524 <nathygirma2524@gmail.com>
2022-04-17 08:01:16 -0700

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,15 @@ create a route
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def index_status():
     """return json"""
     return jsonify(status="OK")
+
+@app_views.route('/stats')
+def count_obj():
+    """count objects"""
+    return jsonify(all=storage.count())
