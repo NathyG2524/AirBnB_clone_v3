f175d4431abdaf984ad20225933db1fbaa892f3a
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-17 08:42:15 -0700

    update  Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,7 +13,16 @@ def index_status():
     """return json"""
     return jsonify(status="OK")
 
+
 @app_views.route('/stats')
 def count_obj():
     """count objects"""
-    return jsonify(all=storage.count())
+    diction_list = {
+        "amenities": storage.count(Amenities),
+        "cities": storage.count(Cities),
+        "places": storage.count(Place),
+        "reviews": storage.count(Reviews),
+        "states": storage.count(States),
+        "users": storage.count(Users)
+    }
+    return jsonify(diction_list)
