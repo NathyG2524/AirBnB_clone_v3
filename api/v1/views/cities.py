e9e6277ffe40548bbd0baf6ff7b9de8f30c5766e
NathyG2524 <nathygirma2524@gmail.com>
2022-04-18 10:37:22 -0700

    pycodestyle fixed

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,6 +10,7 @@ from models import storage
 from models.city import City
 from models.state import State
 
+
 @app_views.route('/states/<state_id>/cities', strict_slashes=False)
 def city_route(state_id):
     """view a city"""
@@ -23,14 +24,16 @@ def city_route(state_id):
             list.append(cities.to_dict())
     return jsonify(list)
 
+
 @app_views.route('/cities/<city_id>')
 def city_id_route(city_id):
     """retrieves the list of all City objects of a state"""
     x = storage.get(City, city_id)
     if x is None:
         abort(404)
-    obj = x.to_dic()
-    return obj
+    obj = x.to_dict()
+    return jsonify(obj)
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
@@ -42,17 +45,23 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
+
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
-def post_city():
+def post_city(state_id):
     """Creates a city"""
-    if not request.get_json():
+    x = storage.get(State, state_id)
+    if not x:
+        abort(404)
+    if not request.get_json(force=True, silent=True):
         return ("Not a JSON", 400)
-    if 'name' is not request.get_json():
-         return ("Missing name", 400)
+    if 'name' not in request.get_json():
+        return ("Missing name", 400)
     new_data = request.get_json()
+    new_data['state_id'] = state_id
     new_city = City(**new_data)
     new_city.save()
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def put_city(city_id):
